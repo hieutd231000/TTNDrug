c64422d35b97e104784f2ea74bd5df12a3b7e52d
hieutd231000 <hieutd2310@gmail.com>
2023-07-08 12:50:14 +0700

    requestModal

--- resources/views/admin/page/inventories/outofstock.blade.php
+++ resources/views/admin/page/inventories/outofstock.blade.php
@@ -19,6 +19,9 @@
         font-size: .875rem;
         line-height: 1.5;
     }
+    .no-resize {
+        resize: none;
+    }
     .m-t-10 {
         margin-top: 10px;
         margin-bottom: 3px;
@@ -86,7 +89,8 @@
                             </div>
                         </form>
                         @foreach($listOutOfStock as $key => $data)
-                            <form id="{{$data->search_product_name}}">
+                            @if(!$data->checkAmount)
+                                <form id="{{$data->search_product_name}}">
                                 <input type="hidden" name="_token" value="{{ csrf_token() }}">
                                 <div class="card-body" style="padding-bottom: 0px">
                                     <div class="card text-center" style="color: #000 !important; background-color: #e9e9e9 !important;">
@@ -95,8 +99,8 @@
                                                 <thead>
                                                 <tr>
                                                     <th scope="col">Mã sản phẩm</th>
+                                                    <th scope="col">Tên sản phẩm</th>
                                                     <th scope="col">DS nhà cung cấp</th>
-                                                    <th scope="col" style="width: 130px">Tên sản phẩm</th>
                                                     <th scope="col">Danh mục</th>
                                                     <th scope="col">Đường dùng</th>
                                                     <th scope="col">Dạng bào chế</th>
@@ -106,6 +110,7 @@
                                                 <tbody>
                                                 <tr>
                                                     <td>{{$data->product_code}}</td>
+                                                    <td>{{$data->product_name}}</td>
                                                     <td>
                                                         @if(count($data->listSupplier))
                                                             @foreach($data->listSupplier as $supplier)
@@ -116,7 +121,6 @@
                                                         @endif
 
                                                     </td>
-                                                    <td>{{$data->product_name}}</td>
                                                     <td>{{$data->category_name}}</td>
                                                     <td>{{$data->route_of_use}}</td>
                                                     <td>{{$data->dosage}}</td>
@@ -133,6 +137,7 @@
                                     </div>
                                 </div>
                             </form>
+                            @endif
                         @endforeach
                     </div>
                 </div>
@@ -140,34 +145,76 @@
         </section>
         <!-- /.content -->
 
-        {{--confirm modal--}}
-        <div class="modal fade" id="confirmModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog" role="document">
+        <!-- Request Product Modal -->
+        <div class="modal fade" id="requestModal" role="dialog" style="display: none;" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered" role="document">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel">Bạn yêu cầu đặt thêm sản phẩm</h5>
+                        <h5 class="modal-title">Thông tin sản phẩm</h5>
                         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
+                            <span aria-hidden="true">×</span>
                         </button>
                     </div>
                     <div class="modal-body">
-                        <div class="card">
-                            <img src="" class="card-img-top" id="card-img-top" alt="...">
-                            <div class="card-body">
-{{--                                <h3 class="product-name" id="product-name" style="margin-bottom: 20px">Card title</h3>--}}
-                                <p class="product-code" id="product-code"></p>
-                                <p class="category" id="category"></p>
-                                <p class="route_of_use" id="route_of_use"></p>
-                                <p class="dosage" id="dosage"></p>
-                                <p class="content" id="content"></p>
-                                <p class="instruction" id="instruction"></p>
-                                <button type="button" class="btn btn-danger" style="float: right" data-dismiss="modal">Thoát</button>
+                        <form>
+                            <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                            <input type="hidden" name="product_id" value="">
+                            <div class="row form-row">
+                                <div class="alert alert-success hidden" id="notification" style="display: inline-block; padding: 9px !important;">
+                                </div>
+                                <div class="col-12">
+                                    <div class="form-group">
+                                        <label>Nhà cung cấp</label>
+{{--                                        <select name="production_batch_selected" id="{{$data[0]->production_batch_id}}" style="width: 60%; margin-top: 8px; height: 30px; margin-bottom: 6px;">--}}
+{{--                                            <option value="" disabled selected>Lô sản xuất</option>--}}
+{{--                                            @foreach($data[0]->production_batch as $key_production_batch => $data_production_batch)--}}
+{{--                                                @if($data_production_batch->expired_status)--}}
+{{--                                                    <option value={{$data_production_batch->production_batch_name}}>{{$data_production_batch->production_batch_name}}</option>--}}
+{{--                                                    --}}{{--                                                                            @else--}}
+{{--                                                    --}}{{--                                                                                <option value={{$data_production_batch->production_batch_name}} disabled>{{$data_production_batch->production_batch_name}}</option>--}}
+{{--                                                @endif--}}
+{{--                                            @endforeach--}}
+{{--                                        </select>--}}
+                                        <div id="help-block-supplier" style="color: red">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-12">
+                                    <div class="form-group">
+                                        <label>Số lượng</label>
+                                        <input class="form-control" name="amount" type="text" value="" placeholder="Số lượng *">
+                                        <div id="help-block-amount" style="color: red">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-12">
+                                    <div class="form-group">
+                                        <label>Ngày yêu cầu</label>
+                                        <input type="text" class="form-control" value="" name="request_time" placeholder="Thời gian yêu cầu *" id="reservationdate"  data-target="#reservationdate" data-toggle="datetimepicker" autocomplete="off"/>
+                                        <div id="help-block-dateRequest" style="color: red">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-12">
+                                    <div class="form-group">
+                                        <label>Mô tả thêm</label>
+                                        <textarea name="detail" id="detail" rows="4" class="form-control no-resize" placeholder="Thông tin thêm"></textarea>
+                                    </div>
+                                </div>
+                                <div class="col-12" style="text-align: end">
+                                    <button type="submit" class="btn btn-primary handleRequest">Yêu cầu</button>
+                                    <button type="button" class="btn btn-danger" data-dismiss="modal">Thoát</button>
+                                </div>
                             </div>
-                        </div>
+{{--                            <button type="button" class="btn btn-danger" style="float: right" data-dismiss="modal">Thoát</button>--}}
+{{--                            <button type="submit" class="btn btn-primary handleRequest">Yêu cầu</button>--}}
+                        </form>
                     </div>
                 </div>
             </div>
         </div>
+        <!-- /Request Product Modal -->
+
     </div>
 @endsection
 
@@ -180,6 +227,18 @@
         $(document).ready(function(){
             $('.alert-edit').fadeIn().delay(2000).fadeOut();
         });
+
+        $('#reservationdate').datetimepicker({
+            format:'DD/MM/YYYY HH:mm:ss',
+            minDate: getFormattedDate(new Date())
+        });
+
+        function getFormattedDate(date) {
+            var day = date.getDate() + 1;
+            var month = date.getMonth() + 1;
+            var year = date.getFullYear().toString().slice(2);
+            return month + '-' + day + '-' + year;
+        }
 
         /**
          * Handle when search box
@@ -200,7 +259,7 @@
         });
 
         function confirmOrder(id) {
-            $("#confirmModal").modal("show");
+            $("#requestModal").modal("show");
         }
     </script>
 @endsection
